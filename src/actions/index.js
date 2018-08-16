3ec5a9c3cd07a8e0230eb48e1f16dabbba674776
Francis Breidenbach <fbreidenbach@gmail.com>
2018-08-15 23:15:26 -0700

    Create SettingsActions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,3 @@
 export * from "./PostActions";
+
+export * from "./SettingsActions";
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,2 +1,5 @@
 // Fetch posts
 export const FETCH_POSTS = "fetch_posts";
+
+// Set results count
+export const SET_RESULTS_COUNT = "set_results_count";
