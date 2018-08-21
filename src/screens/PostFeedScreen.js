f074e2dc222b9e4d6de0cb32fc83df0475448653
Francis Breidenbach <fbreidenbach@gmail.com>
2018-08-20 22:53:23 -0700

    Wrap posts in Cards.

--- src/screens/PostFeedScreen.js
+++ src/screens/PostFeedScreen.js
@@ -13,6 +13,7 @@ import { connect } from "react-redux";
 import MaterialIcon from "react-native-vector-icons/MaterialIcons";
 
 import { fetchPosts } from "../actions";
+import Card from "../components/common/Card";
 import Post from "../components/Post";
 
 class PostFeedScreen extends Component {
@@ -56,14 +57,14 @@ class PostFeedScreen extends Component {
     const { name, profile_image_url_https } = user;
 
     return (
-      <View>
+      <Card>
         <Post
           userImage={profile_image_url_https}
           date={created_at}
           author={name}
           post={text}
         />
-      </View>
+      </Card>
     );
   };
 
