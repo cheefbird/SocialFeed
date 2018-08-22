e18405adb1d1c9379c9432549eaece3753d35b7f
Francis Breidenbach <fbreidenbach@gmail.com>
2018-08-22 00:08:11 -0700

    Shape and display post data.

--- src/screens/PostDetailScreen.js
+++ src/screens/PostDetailScreen.js
@@ -5,13 +5,18 @@ import Card from "../components/common/Card";
 
 export default class PostDetailScreen extends Component {
   render() {
+    const { navigation } = this.props;
+    const post = navigation.getParam("post");
+    const { created_at, user, text } = post;
+    const { name, profile_banner_url } = user;
+
     return (
       <View style={styles.container}>
         <Card>
-          <Image />
-          <Text />
-          <Text />
-          <Text />
+          <Image source={{ uri: profile_banner_url }} style={{ flex: 1 }} />
+          <Text>{created_at}</Text>
+          <Text>{name}</Text>
+          <Text>{text}</Text>
         </Card>
       </View>
     );
